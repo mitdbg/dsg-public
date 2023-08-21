b1c4b06a0e016028239f4b80bdc23a001f781120
Geoffrey Yu <geoffxy@mit.edu>
2023-08-21 11:19:57 -0400

    Address review comments

--- src/components/bibtex.js
+++ src/components/bibtex.js
@@ -1,86 +1,73 @@
-import * as React from "react"
+import React, { useState } from "react"
 
-class Bibtex extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      visible: false
-    }
-    this.handleCopyClick = this.handleCopyClick.bind(this)
-    this.handleToggle = this.handleToggle.bind(this)
-  }
 
-  handleCopyClick() {
-    navigator.clipboard.writeText(this.props.children)
-  }
+const BibtexInner = ({children, handleCopyClick}) => (
+  <div
+    className="bibtex"
+    style={{
+      position: "relative",
+      backgroundColor: "#f5f5f5",
+      padding: "15px",
+      borderRadius: "3px",
+      margin: "15px 0"
+    }}
+  >
+    <pre style={{margin: 0, fontSize: "0.9em"}}>
+      {children}
+    </pre>
+    <button
+      className="bibtex-copy"
+      onClick={handleCopyClick}
+      style={{
+        position: "absolute",
+        top: "10px",
+        right: "10px",
+        cursor: "pointer",
+        backgroundColor: "#aaa",
+        color: "#fff",
+        border: "0",
+        borderRadius: "3px",
+        padding: "5px",
+        outline: "none",
+        fontSize: "0.8em",
+        fontFamily: "sans-serif",
+      }}
+    >
+      Copy
+    </button>
+  </div>
+)
 
-  handleToggle(event) {
-    const {visible} = this.state
-    this.setState({visible: !visible})
+
+const Bibtex = ({children, withToggle}) => {
+  const [visible, setVisible] = useState(false);
+
+  const handleToggle = (event) => {
+    setVisible(!visible)
     event.preventDefault()
   }
 
-  renderBibtex() {
-    return (
-      <div
-        className="bibtex"
-        style={{
-          position: "relative",
-          backgroundColor: "#f5f5f5",
-          padding: "15px",
-          borderRadius: "3px",
-          margin: "15px 0"
-        }}
-      >
-        <pre style={{margin: 0, fontSize: "0.9em"}}>
-          {this.props.children}
-        </pre>
-        <button
-          className="bibtex-copy"
-          onClick={this.handleCopyClick}
-          style={{
-            position: "absolute",
-            top: "10px",
-            right: "10px",
-            cursor: "pointer",
-            backgroundColor: "#aaa",
-            color: "#fff",
-            border: "0",
-            borderRadius: "3px",
-            padding: "5px",
-            outline: "none",
-            fontSize: "0.8em",
-            fontFamily: "sans-serif",
-          }}
-        >
-          Copy
-        </button>
-      </div>
-    )
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(children)
   }
 
-  render() {
-    const {withToggle} = this.props
-    const {visible} = this.state
-
-    if (withToggle) {
-      if (!visible) {
+  if (withToggle) {
+    if (!visible) {
+      return (
+        <>
+          <a href="#" onClick={handleToggle}>View BibTeX</a>
+        </>
+      )
+    } else {
         return (
           <>
-            <a href="#" onClick={this.handleToggle}>View BibTeX</a>
+            <a href="#" onClick={handleToggle}>Hide BibTeX</a>
+            <BibtexInner handleCopyClick={handleCopyClick}>{children}</BibtexInner>
           </>
         )
-      } else {
-          return (
-            <>
-              <a href="#" onClick={this.handleToggle}>Hide BibTeX</a>
-              {this.renderBibtex()}
-            </>
-          )
-      }
-    } else {
-      return this.renderBibtex()
     }
+  } else {
+    return <BibtexInner handleCopyClick={handleCopyClick}>{children}</BibtexInner>
   }
 }
 
